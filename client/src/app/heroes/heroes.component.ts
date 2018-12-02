3b5d1ed1244f5146d431938303fc4cee2d16af50
Pedro Souza <henrique01311@gmail.com>
2018-12-02 02:18:53 -0200

    Atividade Completa

---
+++ client/src/app/heroes/heroes.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { Hero } from '../hero';
+import { listHero } from '../mock-heroes';
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.css']
+})
+export class HeroesComponent implements OnInit {
+
+  heroes = listHero;
+  selectedHero: Hero;
+  
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ client/src/app/mock-heroes.ts
@@ -0,0 +1,15 @@
+import { Hero } from "./hero";
+
+
+export const listHero: Hero[] =[
+    {
+        id : 1,
+        name : "Pedro"
+      },{
+        id: 2,
+        name : "Super Homem"
+      }, {
+        id : 3,
+        name : "Homem Formiga"
+      }
+];
